b2d95341d1a7135e0449a6a74e0bf80330002728
Jose Luis <jlrc23@gmail.com>
2020-09-12 23:59:15 -0500

    usando takewhile

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,8 +1,17 @@
+import { fromEvent } from 'rxjs';
 import { displayLog } from './utils';
+import { map, filter, take, takeWhile } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
+
+    const click$ = fromEvent(grid, 'click').pipe(
+        map(val=>[Math.floor(val.offsetX /50), Math.floor(val.offsetY/50)]),
+        takeWhile([col, row]=>col>3)
+    );
+    const subsClick = click$.subscribe(displayLog);
+
 
 
     /** end coding */
