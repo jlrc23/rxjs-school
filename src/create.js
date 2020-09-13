887f172fe236d084108e9c2a8c618d0b2ff91686
Jose Luis <jlrc23@gmail.com>
2020-09-12 23:38:05 -0500

    usando timer y interval

--- src/create.js
+++ src/create.js
@@ -1,14 +1,12 @@
 import { displayLog } from './utils';
-import { Observable } from 'rxjs';
+import { interval, timer } from 'rxjs';
 
 export default () => {
-    const hello = Observable.create(function(observer){
-        observer.next("Hello");
-        setTimeout(()=>{
-            observer.next("world");
-        }, 1000);
-        
-    });
+    const source = interval(500);
+    const subscription = source.subscribe(data=>displayLog(data));
+    timer(3000).subscribe(()=>subscription.unsubscribe());
 
-    const subscribe = hello.subscribe(evt=> displayLog(evt))
+    const source2 = timer(4000, 100);
+    const subs2 = source2.subscribe(data=>displayLog(`2- ${data}`));
+    timer(5000).subscribe(()=>subs2.unsubscribe());
 }
