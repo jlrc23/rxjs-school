5f0f2c7e45b34181e82cec919849dcf213d3ae32
Jose Luis <jlrc23@gmail.com>
2020-10-05 09:04:02 -0500

    Diferencia entre mergeMap, switchMap y concatMap

--- src/api.js
+++ src/api.js
@@ -3,8 +3,9 @@ import { mapTo } from 'rxjs/operators';
 
 export class api{
     static getComment(id){
-        return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
+        const numero=id;
+        return timer(Math.random()*3000).pipe(
+            mapTo({id:id, comment:`comment number ${numero}`})
         );
     }
 }
